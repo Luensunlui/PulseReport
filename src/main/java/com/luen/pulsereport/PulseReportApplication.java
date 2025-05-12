7553a5ed354f830f1103e2e89c5fe04fb6110982
luen <614036039@qq.com>
2025-05-12 21:45:58 +0800

    initial

---
+++ src/main/java/com/luen/pulsereport/PulseReportApplication.java
@@ -0,0 +1,14 @@
+package com.luen.pulsereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PulseReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luen/pulsereport/PulseReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luen.pulsereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
